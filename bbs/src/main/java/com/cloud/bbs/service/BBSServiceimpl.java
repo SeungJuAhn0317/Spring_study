2435ec0b02b23670ee50b633cde8bd10a0e0a319
SeungJuAhn0317 <asj7109@naver.com>
2021-07-06 16:32:38 +0900

    게시판 완료.

--- bbs/src/main/java/com/cloud/bbs/service/BBSServiceimpl.java
+++ bbs/src/main/java/com/cloud/bbs/service/BBSServiceimpl.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cloud.bbs.common.FileSaveHelper;
@@ -49,6 +50,7 @@ public class BBSServiceimpl implements BBSService {
 		}		
 	}
 
+	@Transactional()
 	@Override
 	public void write(BBSDto article,List<MultipartFile> fileUpload) {		
 		//		List<MultipartFile>는 CommonsMultipartFile을 리턴함
